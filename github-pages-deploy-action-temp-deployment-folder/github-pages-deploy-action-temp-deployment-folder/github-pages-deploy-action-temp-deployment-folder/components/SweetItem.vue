a0f436a9304764ba723b3090e3175c0ea6a6124c
ChHiro <hir.cho22@gmail.com>
2022-04-24 14:39:05 +0000

    Deploying to gh-pages from  @ a319284bd4496740c4fb61e26159877e215d6a75 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/components/SweetItem.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    setup() {
+        const sweetName = "name";
+
+        return {
+            sweetName,
+        };
+    },
+};
+</script>
+
+<template>
+    <div>aa{{ sweetName }}bb</div>
+</template>
